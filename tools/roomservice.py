c2d7edb3425102614495cb63584d6b8270dd2fc6
Joshua Lynch <jl52741@gmail.com>
2016-01-29 04:26:47 -0600

    Fix this

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -41,11 +41,11 @@ default_rem = "raindrop"
 default_rev = "android-6.0"
 # set this to the remote that you use for projects from your team repos
 # example fetch="https://github.com/RaindropOS"
-default_team_rem = "raindropos"
+default_team_rem = "raindrop"
 # this shouldn't change unless google makes changes
 local_manifest_dir = ".repo/local_manifests"
 # change this to your name on github (or equivalent hosting)
-android_team = "raindrop"
+android_team = "RaindropOS"
 # url to gerrit repository
 # gerrit_url = "gerrit.raindrop.org"
 
